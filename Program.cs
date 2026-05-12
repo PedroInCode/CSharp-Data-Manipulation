2e3bc28e85255770690fd859eca1565cc9725023
Pedro Gustavo <pedrosystems.dev@gmail.com>
2026-05-12 17:10:47 -0300

    feat: setup inicial da solução de manipulação de dados

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+
+var diasDaSemana = new string[] { "Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado" };
+
+//var carrinho = new ArrayList()
+//-> Problema: ArrayList é uma coleção não genérica, o que significa que ela pode conter objetos de qualquer tipo. Isso pode levar a problemas de segurança de tipo e desempenho, pois é necessário fazer castings para acessar os elementos corretamente. 
+
+var carrinho = new List<Produto>()
+{
+    new Produto { Nome = "Leite", Preco = 7.89 },
+    new Produto { Nome = "Manteiga", Preco = 3.45 },
+};
+
+for (int i=0; i < carrinho.Count; i++)
+{
+    Console.WriteLine($"Produto: {carrinho[i].Nome}");
+}
+
+
+class Produto
+{
+    public string Nome { get; set; }
+    public double Preco { get; set; }
+}
