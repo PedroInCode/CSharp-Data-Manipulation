48ab76443ee10c309dbf8fdb3c004b8b18bc1bfc
Pedro Gustavo <pedrosystems.dev@gmail.com>
2026-05-14 17:07:23 -0300

    feat: reorganizando estrutura da solução e adicionando projetos do curso

---
+++ 02-OperadoresEmColecoes/Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+livremente a qualquer momento. Além disso, o aplicativo precisa oferecer a funcionalidade de
+reprodução aleatória para uma playlist específica, proporcionando uma experiência de audição
+dinâmica e variada, sem, contudo, alterar a ordem original que o usuário definiu. O desafio
+é criar uma estrutura robusta que suporte a adição e remoção eficiente de músicas, a
+reordenação flexível dentro das playlists e a seleção de faixas tanto em modo sequencial
+quanto aleatório.
+
+
+Funcoes que vamos implementar:
+// [ ] Criar as classes para musicas e playlist
+// [ ] Listar musicas da playlist
+// [ ] Adicionar musica à playlist
+// [ ] Obter uma musica especifica da playlist
+// [ ] Remover musica da playlist
+// [ ] Tocar uma musica aleatoria da playlist
+// [ ] Reordenar musicas segundo alguma logica especifica (ex. duracao)
+// [ ] Uma playlist nao pode ter musicas repetidas
+// [ ] Exibir as 10 musicas mais tocadas em todas as playlists (ranking)
+// [ ] Player de musica com:
+// [ ] - Fila de reproducao (para musicas avulsas e/ou playlists)
+// [ ] - Historico de reproducao */
+
+var musica1 = new Musica { Titulo = "Qua Pais É Esse", Artista = "Legião Urbana", Duracao = 350 };
+var musica2 = new Musica { Titulo = "Tempo Perdido", Artista = "Legião Urbana", Duracao = 455 };
+var musica3 = new Musica { Titulo = "Pro dia nascer feliz", Artista = "Barão Vermelho", Duracao = 345 };
+var musica4 = new Musica { Titulo = "Eduardo e Mônica", Artista = "Legião Urbana", Duracao = 530 };
+var musica5 = new Musica { Titulo = "Geração Coca-Cola", Artista = "Legião Urbana", Duracao = 350 };
+
+var rockNacional = new Playlist { Nome = "Rock Nacional" };
+
+class Musica
+{
+    public string Titulo { get; set; }
+    public string Artista { get; set; }
+    public int Duracao { get; set; }
+}
+class Playlist
+{
+    public string Nome { get; set; }
+}
