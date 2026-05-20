b4b4d7c6ace9232d70f8c918708bd8fb45bac921
Pedro Gustavo <pedrosystems.dev@gmail.com>
2026-05-20 17:28:12 -0300

    feat: implementa leitura eficiente de musicas com StreamReader e IEnumerable

--- 03-AbstraindoAFonteDeDados/Program.cs
+++ 03-AbstraindoAFonteDeDados/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using var arquivo = new FileStream("musicas.csv", FileMode.Open, FileAccess.Read);
+using var strean = new StreamReader(arquivo);
+
+var musicas = ObterMusicas(strean);
+ExibirMusicas(musicas);
+
+void ExibirMusicas(IEnumerable<Musica> musicas)
+{
+    var contador = 1;
+    Console.WriteLine("\nExbindo as Músicas:");
+    foreach (var musica in musicas)
+    {
+        Console.WriteLine($"\t - {musica.Titulo}");
+        contador ++;
+        if (contador > 10) break; // Limita a exibição a 10 músicas
+
+    }
+}
+
+IEnumerable<Musica> ObterMusicas(StreamReader stream)
+{
+    var linha = stream.ReadLine(); // Lê a primeira linha (cabeçalho)
+    while (linha is not null) // Continua lendo até o final do arquivo
+    {
+        var partes = linha.Split(';'); // Divide a linha em partes usando o ponto e vírgula como separador
+        var musica = new Musica
+        {
+            Titulo = partes[0],
+            Artista = partes[1],
+            Duracao = Convert.ToInt32(partes[2])
+        };
+        yield return musica; // Retorna a música atual e pausa a execução
+        linha = stream.ReadLine(); // Lê a próxima linha
+    }
+}
+
+
+class Musica
+{
+    public string Titulo { get; set; }
+    public string Artista { get; set; }
+    public int Duracao { get; set; } // Duração em segundos
+    
+}
