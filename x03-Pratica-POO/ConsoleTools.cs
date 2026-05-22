bd83a141a5d2bd7b6d4e54c495d651d7c65fcdec
Pedro Gustavo <pedrosystems.dev@gmail.com>
2026-05-22 20:03:15 -0300

    feat: conecta as peças no Program e finaliza leitura e exibição de musicas

---
+++ x03-Pratica-POO/ConsoleTools.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace x03_Pratica_POO;
+
+internal class ConsoleTools
+{
+    public void ExibirMusicas(IEnumerable<Musica> musicas)
+    {
+        var cont = 1;
+        Console.WriteLine("Exibindo músicas:");
+        foreach (var musica in musicas)
+        {
+            Console.WriteLine($"\t - {musica.Titulo} - {musica.Artista} ({musica.Duracao} segundos)"); 
+            cont++;
+
+            if (cont > 10)
+            {
+                Console.WriteLine("\t - ...");
+                break;
+            }
+        }
+    }
+}
--- x03-Pratica-POO/Program.cs
+++ x03-Pratica-POO/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using x03_Pratica_POO;
+
+using var stream = new FileStream("musicas.csv", FileMode.Open, FileAccess.Read);
+using var reader = new StreamReader(stream);
+
+var leitor = new LeitorArquivo();
+var Exibidor = new ConsoleTools();
+
+var musicas = leitor.ObterMusicas(reader);
+Exibidor.ExibirMusicas(musicas);
+
