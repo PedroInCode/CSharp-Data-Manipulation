29d370c0d1a5dca83d8a307f0c5da608d5c397f6
Pedro Gustavo <pedrosystems.dev@gmail.com>
2026-05-22 19:49:04 -0300

    feat: isola classe Musica e cria classe LeitorArquivo aplicando os principios de POO

---
+++ x03-Pratica-POO/LeitorArquivo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.Marshalling;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace x03_Pratica_POO;
+
+internal class LeitorArquivo
+{
+    public IEnumerable<Musica> ObterMusicas(StreamReader stream)
+    {
+        var linha = stream.ReadLine();
+
+        while (linha is not null)
+        {
+            var partes = linha.Split(";");
+
+            var musica = new Musica
+            {
+                Titulo = partes[0],
+                Artista = partes[1],
+                Duracao = int.Parse(partes[2])
+            };
+
+            yield return musica;
+            linha = stream.ReadLine();
+        }
+    }
+}
---
+++ x03-Pratica-POO/Musica.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace x03_Pratica_POO;
+
+internal class Musica
+{
+    public string Titulo { get; set; }
+    public string Artista { get; set; }
+    public int Duracao { get; set; } // Duração em segundos
+}
