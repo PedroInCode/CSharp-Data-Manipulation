3b4e09239b3dd0b5b09d750e53701b12c405641a
Pedro Gustavo <pedrosystems.dev@gmail.com>
2026-05-25 16:37:22 -0300

    feat: implementa a primeira consulta filtrada usando LINQ (.Where)

--- 04-LINQ/Program.cs
+++ 04-LINQ/Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using var arquivo = new FileStream("musicas.csv", FileMode.Open, FileAccess.Read);
+using var strean = new StreamReader(arquivo);
+
+
+var musicasDoArtista =
+    ObterMusicas(strean)                             // 1. Obtém as músicas do arquivo CSV
+    .Where(musica => musica.Artista == "Metallica")            // 2. Filtragem por artista usando o método de extensão
+    .Where(musica => musica.Duracao >= 400);              // 3. Filtragem por duracao usando o metodo de extensão
+ExibirMusicas(musicasDoArtista);
+
+// ----------------------------------------------------------------------------------------------------------------- //
+
+// Método que exibe as músicas no console
+void ExibirMusicas(IEnumerable<Musica> musicas)
+{
+    var contador = 1;
+    Console.WriteLine("\nExbindo as Músicas:");
+    foreach (var musica in musicas)
+    {
+        Console.WriteLine($"\t - {musica.Titulo} ({musica.Artista}) - {musica.Duracao} segundos");
+        contador++;
+        if (contador > 10) break; // Limita a exibição a 10 músicas
+
+    }
+}
+
+// Método que lê as músicas de um arquivo CSV e retorna um IEnumerable<Musica>
+IEnumerable<Musica> ObterMusicas(StreamReader stream)
+{
+    var linha = stream.ReadLine(); // Lê a primeira linha (cabeçalho)
+    while (linha is not null) // Continua lendo até o final do arquivo
+    {
+        var partes = linha.Split(';'); // Divide a linha em partes usando o ponto e vírgula como separador
+        var musica = new Musica
+        {
+            Titulo = partes[0],
+            Artista = partes[1],
+            Duracao = Convert.ToInt32(partes[2])
+        };
+        yield return musica; // Retorna a música atual e pausa a execução
+        linha = stream.ReadLine(); // Lê a próxima linha
+    }
+}
+
+
+// Classes
+
+class Musica
+{
+    public string Titulo { get; set; }
+    public string Artista { get; set; }
+    public int Duracao { get; set; } // Duração em segundos
+
+}
