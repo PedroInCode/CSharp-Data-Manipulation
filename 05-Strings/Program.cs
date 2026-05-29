21b2ef79eabd509325dfbaf4e0e582b322465671
Pedro Gustavo <pedrosystems.dev@gmail.com>
2026-05-29 16:40:01 -0300

    feat: implementa leitura do CSV com yield return no modulo 05

--- 05-Strings/Program.cs
+++ 05-Strings/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using var arquivo = new FileStream("musicas.csv", FileMode.Open, FileAccess.Read);
+using var stream = new StreamReader(arquivo);
+
+var musicas = ObterMusicas(stream); // Obtém as músicas do arquivo CSV
+ExibirMusicas(musicas); // Exibe as músicas no console
+
+void ExibirMusicas(IEnumerable<Musica> musicas)
+{
+    var contador = 1;
+    Console.WriteLine("\nExbindo as Músicas:");
+    foreach (var musica in musicas)
+    {
+        Console.WriteLine($"\t - {musica.Titulo} ({musica.Artista}) - {musica.Duracao} segundos - Lançamento: {musica.DataLancamento.ToShortDateString()}");
+        contador++;
+        if (contador > 20) break; // Limita a exibição a 10 músicas
+
+    }
+}
+
+// Método que lê as músicas de um arquivo CSV e retorna um IEnumerable<Musica>
+
+IEnumerable<Musica> ObterMusicas(StreamReader stream)
+{
+    var linha = stream.ReadLine();                               // Lê a primeira linha (cabeçalho)
+    while (linha is not null)                                   // Continua lendo até o final do arquivo
+    {
+        var partes = linha.Split(';');                        // Divide a linha em partes usando o ponto e vírgula como separador
+        var musica = new Musica
+        {
+        Titulo = partes[0],
+        Artista = partes[1],
+        Duracao = Convert.ToInt32(partes[2]),
+        Generos = partes[3].Split(",").Select(g => g.Trim()),   
+        DataLancamento = DateTime.Parse(partes[4])
+        };
+            yield return musica;                          // Retorna a música atual e pausa a execução
+            linha = stream.ReadLine();                   // Lê a próxima linha
+    }
+}
+
+// Classes
+class Musica
+{
+    public string Titulo { get; set; }
+    public string Artista { get; set; }
+    public int Duracao { get; set; } // Duração em segundos
+    public IEnumerable<string> Generos { get; set; }
+    public DateTime DataLancamento { get; set; }
+
+}
